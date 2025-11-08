084d7d1db14dba65fbd8f3ef598357eb2aa790a2
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-08 22:20:00 +0530

    update

--- products/models.py
+++ products/models.py
@@ -4,14 +4,19 @@ from base.models import BaseModel
 # Create your models here.
 class Category(BaseModel):
     category_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     category_image = models.ImageField(upload_to='categories/')
+
 
 
 class Product(BaseModel):
     product_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
     price = models.IntegerField()
     product_description = models.TextField()
+
+
 
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='product_image')
