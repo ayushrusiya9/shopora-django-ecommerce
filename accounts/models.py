caf5839b8e228d60bfba524b13ae54445d9244a3
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-10 20:24:10 +0530

    update

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from base.models import BaseModel
+import uuid
 
-# Create your models here.
 class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     is_email_verified = models.BooleanField(default=False)
-    email_token = models.ImageField(upload_to='profile')
+    email_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    profile_image = models.ImageField(upload_to='profile/', blank=True, null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def login_page(request):
     return render(request, 'accounts/login.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=email).exists():
+            messages.warning(request, 'Email is already registered!')
+            return HttpResponseRedirect(request.path_info)
+
+        #create_user() hashes password automatically and sets username
+        user_obj = User.objects.create_user(
+            username=email,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
+        )
+
+        messages.success(request, 'Account created successfully!')
+        return HttpResponseRedirect('/account/login/')  # redirect to login page
+
+    return render(request, 'accounts/register.html')
