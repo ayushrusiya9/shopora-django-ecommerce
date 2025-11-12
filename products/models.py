5d829753fd0b46f9b735e247c0ece09bd80a8f49
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-12 20:17:24 +0530

    update

--- products/models.py
+++ products/models.py
@@ -1,23 +1,46 @@
 from django.db import models
+from django.utils.text import slugify
 from base.models import BaseModel
 
-# Create your models here.
-class Category(BaseModel):
-    category_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, null=True, blank=True)
-    category_image = models.ImageField(upload_to='categories/')
 
+class Category(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to='categories/')
+
+    def save(self, *args, **kwargs):
+        # auto generate slug only if empty
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(BaseModel):
-    product_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
-    price = models.IntegerField()
-    product_description = models.TextField()
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    is_available = models.BooleanField(default=True)
 
+    def save(self, *args, **kwargs):
+        # auto generate slug only if empty
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class ProductImage(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='product_image')
-    image = models.ImageField(upload_to='product')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
