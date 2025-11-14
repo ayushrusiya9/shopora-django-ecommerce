8373e958339d225edfe74ae7584e5d4d3a30f595
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-14 19:39:10 +0530

    update

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product,ProductImage
 
 # Create your views here.
 def home(request):
-
-    context = {"products": Product.objects.all()}
+    context = {"products": Product.objects.all(),"product_image":ProductImage.objects.all()}
+    print()
     return render(request, 'home/index.html',context)
